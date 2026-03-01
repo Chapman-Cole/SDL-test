89364b51ebd7e4e412165f7ed984ca6491a2abfb
Cole Chapman <colechapman924@gmail.com>
2026-02-28 17:05:08 -0800

    Reworked gpu buffer creation and submission to gpu

--- engine/include/GPUBuffers.h
+++ engine/include/GPUBuffers.h
@@ -3,67 +3,34 @@
 
 #include <SDL3/SDL.h>
 
-// Transfer buffers of set sizes will be cached and used for uploading data to the gpu for vertex buffers and
-// index buffers. If any of the pre-existing sizes are too small for the data requested to be uploaded, then a new
-// transfer buffer will be appended of the required size and cached for use later if neeeded
-typedef struct GPBUploadBuffer {
-    SDL_GPUTransferBuffer* utb;
-    Uint32 size;
-    bool available;
-} GPBUploadBuffer;
+typedef struct TBStackElement {
+    SDL_GPUTransferBuffer* transferBuffer;
+    SDL_GPUBuffer* buffer;
+    Uint32 dataSize;
+} TBStackElement;
 
-// Defines the possible sizes of the transfer buffers, with each size being
-// allocated 4 transfer buffers each
-// Note: these sizees should definitely be updated later
-static const Uint32 transferBufferBinSizes[] = {6400, 64000, 256000, 1024000, 16384000};
-#define TRANSFER_BUFFER_BIN_LEN sizeof(transferBufferBinSizes) / sizeof(transferBufferBinSizes[0])
-#define TRANSFER_BUFFER_BIN_SPAN 4
+static Uint32 TBStackLen = 0;
+static Uint32 TBStackCapacity = 1;
+static TBStackElement* TBStack = NULL;
 
-// The actual array of transfer buffers (will be of size TRANSFER_BUFFER_BIN_LEN * TRANSFER_BUFFER_BIN_SPAN)
-static GPBUploadBuffer* uploadTransferBuffers;
-static Uint32 uploadTransferBuffersSize = 0;
+// Must call to setup the TBStack array
+int GPB_init(void);
 
-// For use with the stack to store information relavent to the copy pass
-typedef struct upload_info {
-    SDL_GPUTransferBufferLocation location;
-    SDL_GPUBufferRegion region;
-    GPBUploadBuffer* buffer;
-} upload_info;
+// Must be called to free up the memory used by the TBStack array
+int GPB_terminate(void);
 
-// This will serve as the transfer buffer stack
-static upload_info* uploadStack = NULL;
-static Uint32 uploadStackSize = 0;
-static Uint32 uploadStackMemsize = 1;
+// Pushes an element to the TBStack
+int GPB_TBStack_push(TBStackElement element);
 
-// Must be called before using any of the following functions
-// sets the internal gpu device to whatever is passed into the function and sets up
-// the upload transfer buffer array with some of the default sizes
-void GPB_init();
+// Pops an element off the TBStack, which has the effect of freeing the used transfer buffer
+// once it is done using it
+int GPB_TBStack_pop(void);
 
-// Must be called at the end of the program for proper de-initialization
-// the primary thing it does is free up transfer buffers
-void GPB_terminate(void);
-
-// Creates a gpu buffer of the specified type using the specified data
-// Note: type should be something like SDL_GPU_BUFFERUSAGE_*
-// where * is VERTEX, INDEX, etc.
-// Note: the size should be the number of bytes that the buffer is
+// Creates an SDL_GPUBuffer* and adds it to an internal stack for submitting to the gpu later on
 SDL_GPUBuffer* GPB_create_buffer(Uint8 type, void* data, Uint32 size);
-
-// pushes a transfer buffer onto the transfer buffer stack
-int GPB_push_uinfo(upload_info* uinfo);
-
-// Pops off a value at the end of the transfer buffer stack (and properly manages memory)
-int GPB_pop_uinfo(void);
 
 // Starts a copy pass to submit all of the created transfer buffers
 // it then frees up all of the created transfer buffers once they are not needed
 int GPB_submit_all_transfer_buffers(void);
-
-// Returns a transfer buffer that is large enough for the given size. If the given size is larger than any
-// of the existing transfer buffers, a new transfer buffer will be appended to the array of transfer buffers
-// with a size large enough to fit the new size. If it runs out of space, then the program will crash and print
-// out an error to the terminal
-GPBUploadBuffer* GPB_get_transfer_buffer(Uint32 size);
 
 #endif
--- engine/src/GPUBuffers.c
+++ engine/src/GPUBuffers.c
@@ -2,160 +2,157 @@
 #include <stdlib.h>
 #include "SDLDevice.h"
 
-void GPB_init() {
-    uploadTransferBuffers = (GPBUploadBuffer*)SDL_malloc((TRANSFER_BUFFER_BIN_LEN * TRANSFER_BUFFER_BIN_SPAN) * sizeof(GPBUploadBuffer));
+int GPB_init(void) {
+    TBStack = (TBStackElement*)SDL_malloc(TBStackCapacity * sizeof(TBStackElement));
 
-    if (uploadTransferBuffers == NULL) {
-        SDL_Log("GPUBuffers.c: Failed to allocate memory for the upload transfer buffers.");
+    if (TBStack == NULL) {
+        SDL_Log("Failed to allocate memory for TBStack in GPUBuffers.c");
         SDL_Quit();
         exit(-1);
     }
 
-    for (int i = 0; i < TRANSFER_BUFFER_BIN_LEN; i++) {
-        for (int j = 0; j < TRANSFER_BUFFER_BIN_SPAN; j++) {
-            uploadTransferBuffers[(i * TRANSFER_BUFFER_BIN_SPAN) + j] = (GPBUploadBuffer){
-                .utb = SDL_CreateGPUTransferBuffer(
-                    get_SDL_gpu_device(),
-                    &(SDL_GPUTransferBufferCreateInfo){
-                        .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
-                        .size = transferBufferBinSizes[i],
-                        .props = 0}),
-                .available = true,
-                .size = transferBufferBinSizes[i]};
-        }
-    }
+    return 0;
 }
 
-void GPB_terminate(void) {
-    for (int i = 0; i < uploadTransferBuffersSize; i++) {
-        SDL_ReleaseGPUTransferBuffer(get_SDL_gpu_device(), uploadTransferBuffers[i].utb);
+int GPB_terminate(void) {
+    for (int i = 0; i < TBStackLen; i++) {
+        SDL_ReleaseGPUTransferBuffer(get_SDL_gpu_device(), TBStack[i].transferBuffer);
+    }
+    SDL_free(TBStack);
+
+    return 0;
+}
+
+int GPB_TBStack_push(TBStackElement element) {
+    if (TBStackLen + 1 >= TBStackCapacity) {
+        TBStackCapacity *= 2;
+
+        TBStack = (TBStackElement*)SDL_realloc(TBStack, TBStackCapacity * sizeof(TBStackElement));
+
+        if (TBStack == NULL) {
+            SDL_Log("Failed to expand capacity of TBStack in GPUBuffers.c");
+            SDL_Quit();
+            exit(-1);
+        }
     }
 
-    SDL_free(uploadTransferBuffers);
-    SDL_free(uploadStack);
+    TBStack[TBStackLen] = element;
+    TBStackLen++;
+
+    return 0;
+}
+
+int GPB_TBStack_pop(void) {
+    SDL_ReleaseGPUTransferBuffer(get_SDL_gpu_device(), TBStack[TBStackLen-1].transferBuffer);
+
+    if (TBStackLen - 1 < TBStackCapacity / 2) {
+        TBStackCapacity /= 2;
+        if (TBStackCapacity <= 0) {
+            TBStackCapacity = 1;
+        }
+
+        TBStack = (TBStackElement*)SDL_realloc(TBStack, TBStackCapacity * sizeof(TBStackElement));
+
+        if (TBStack == NULL) {
+            SDL_Log("Failed to shrink size of capacity of TBStack in GPUBuffers.c");
+            SDL_Quit();
+            exit(-1);
+        }
+    }
+
+    TBStackLen--;
+
+    return 0;
 }
 
 SDL_GPUBuffer* GPB_create_buffer(Uint8 type, void* data, Uint32 size) {
-    // Create the gpu buffer
-    SDL_GPUBuffer* buffer = SDL_CreateGPUBuffer(
+    SDL_GPUBuffer* buffer = NULL;
+    SDL_GPUTransferBuffer* transferBuffer = NULL;
+
+    // The buffers are created below
+    buffer = SDL_CreateGPUBuffer(
         get_SDL_gpu_device(),
         &(SDL_GPUBufferCreateInfo){
-            .usage = type,
             .size = size,
-            .props = 0});
+            .usage = type,
+            .props = 0
+        }
+    );
 
-    // Create the transfer buffer for getting the data to the gpu buffer
-    GPBUploadBuffer* tb = GPB_get_transfer_buffer(size);
+    if (buffer == NULL) {
+        SDL_Log("Failed to create GPU Buffer in GPUBuffers.c");
+        SDL_Quit();
+        exit(-1);
+    }
 
-    // Fill the transfer buffer with data by mapping it to a pointer
-    void* tb_data = SDL_MapGPUTransferBuffer(get_SDL_gpu_device(), tb->utb, false);
-    SDL_memcpy(tb_data, data, size);
-    SDL_UnmapGPUTransferBuffer(get_SDL_gpu_device(), tb->utb);
+    transferBuffer = SDL_CreateGPUTransferBuffer(
+        get_SDL_gpu_device(),
+        &(SDL_GPUTransferBufferCreateInfo){
+            .size = size,
+            .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+            .props = 0
+        }
+    );
 
-    // Find where the data is
-    SDL_GPUTransferBufferLocation location = {0};
-    location.transfer_buffer = tb->utb;
-    location.offset = 0;
+    if (transferBuffer == NULL) {
+        SDL_Log("Failed to create transfer buffer in GPUBuffers.c");
+        SDL_Quit();
+        exit(-1);
+    }
+    
+    // Fill the transfer buffer with the data
+    void* gpuData = (void*)SDL_MapGPUTransferBuffer(get_SDL_gpu_device(), transferBuffer, false);
 
-    // Find where to upload the data
-    SDL_GPUBufferRegion region = {0};
-    region.buffer = buffer;
-    region.size = size;
-    region.offset = 0;
+    if (gpuData == NULL) {
+        SDL_Log("Failed to map transfer buffer to memory space in GPUBuffers.c");
+        SDL_Quit();
+        exit(-1);
+    }
 
-    GPB_push_uinfo(&(upload_info){
-        .location = location,
-        .region = region,
-        .buffer = tb});
+    SDL_memcpy(gpuData, data, size);
+    SDL_UnmapGPUTransferBuffer(get_SDL_gpu_device(), transferBuffer);
+
+    GPB_TBStack_push((TBStackElement){
+        .buffer = buffer,
+        .transferBuffer = transferBuffer,
+        .dataSize = size
+    });
 
     return buffer;
 }
 
-int GPB_push_uinfo(upload_info* uinfo) {
-    if (uploadStackSize + 1 >= uploadStackMemsize) {
-        uploadStackMemsize *= 2;
-        void* test = SDL_realloc(uploadStack, uploadStackMemsize * sizeof(upload_info));
-        if (test == NULL) {
-            SDL_Log("Failed to allocate memory for the transfer buffer stack\n");
-            SDL_Quit();
-            exit(-1);
-        }
-        uploadStack = test;
-    }
-
-    uploadStack[uploadStackSize] = *uinfo;
-    uploadStackSize++;
-    return 0;
-}
-
-int GPB_pop_uinfo(void) {
-    if (uploadStackSize > 0) {
-        uploadStackSize--;
-        // Make the transfer buffer available for new data again
-        uploadStack[uploadStackSize].buffer->available = true;
-    }
-    return 0;
-}
-
 int GPB_submit_all_transfer_buffers(void) {
-    // Start a copy pass to get the data in the transfer buffer to the gpu buffers
     SDL_GPUCommandBuffer* commandBuffer = SDL_AcquireGPUCommandBuffer(get_SDL_gpu_device());
-    SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(commandBuffer);
-
-    for (int i = uploadStackSize - 1; i >= 0; i--) {
-        SDL_UploadToGPUBuffer(copyPass, &uploadStack[i].location, &uploadStack[i].region, true);
-        GPB_pop_uinfo();
-    }
-
-    // End the copy pass
-    SDL_EndGPUCopyPass(copyPass);
-    SDL_SubmitGPUCommandBuffer(commandBuffer);
-
-    return 0;
-}
-
-GPBUploadBuffer* GPB_get_transfer_buffer(Uint32 size) {
-    GPBUploadBuffer* tb = NULL;
-
-    // Calculate the offset into the correct size
-    int offset = -1;
-    for (int i = 0; i < TRANSFER_BUFFER_BIN_LEN; i++) {
-        if (size <= transferBufferBinSizes[i]) {
-            offset = i * TRANSFER_BUFFER_BIN_SPAN;
-        }
-    }
-
-    // Check to make sure that it actually fit into one of the transfer buffer categories
-    if (offset == -1) {
-        SDL_Log("GPUBuffers: GPB_get_transfer_buffer was given a size larger than the specified bin sizes\n");
+    if (commandBuffer == NULL) {
+        SDL_Log("Failed to create command buffer for copy pass in GPUBuffers.c");
         SDL_Quit();
         exit(-1);
     }
 
-    int escape_con = 0;
-search_again:
-    // Find the first available transfer buffer in that range, if there is one available at all
-    for (int i = offset; i < offset + TRANSFER_BUFFER_BIN_SPAN; i++) {
-        if (uploadTransferBuffers[i].available == true) {
-            tb = &uploadTransferBuffers[i];
-        }
-    }
-    // Indicate that the buffer is no longer available (helps prevent accidental overwriting of buffers)
-    tb->available = false;
-    
-    if (tb == NULL) {
-        // If no slots of the specified size are available, it will submit everything that is already in the queue
-        GPB_submit_all_transfer_buffers();
+    SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(commandBuffer);
 
-        if (escape_con == 0) {
-            goto search_again;
-            escape_con++;
-        } else {
-            SDL_Log("GPUBuffers.c: Infinite loop would have been entered\n");
-            SDL_Quit();
-            exit(-1);
-        }
+    for (int i = 0; i < TBStackLen; i++) {
+        SDL_UploadToGPUBuffer(
+            copyPass,
+            &(SDL_GPUTransferBufferLocation){
+                .transfer_buffer = TBStack[i].transferBuffer,
+                .offset = 0
+            },
+            &(SDL_GPUBufferRegion){
+                .buffer = TBStack[i].buffer,
+                .size = TBStack[i].dataSize,
+                .offset = 0
+            },
+            true
+        );
     }
 
-    return tb;
+    SDL_EndGPUCopyPass(copyPass);
+    SDL_SubmitGPUCommandBuffer(commandBuffer);
+
+    while (TBStackLen > 0) {
+        GPB_TBStack_pop();
+    }
+
+    return 0;
 }
